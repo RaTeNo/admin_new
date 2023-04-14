c4038826965ba0f20d6a213d8632d8eb403ca4e4
RaTeNo <rateno@mail.ru>
2023-04-14 18:09:54 +0300

    аааа

--- js/simulator.js
+++ js/simulator.js
@@ -19,3 +19,21 @@ $(document).ready(function() {
 
    
 });
+
+$(window).on('resize', () => {
+    if($(window).width()>1023)
+    {
+        const elHeight = $(".data_with_sidebar .data").outerHeight();
+        $(".data_with_sidebar .steps").outerHeight(elHeight);
+
+
+        if($(".data_with_sidebar .steps").length>0)
+        {   
+            
+                var hash = window.location.hash;
+                var top = $(hash).position().top; // получаем координаты блока
+                $('.inner_steps').animate({scrollTop: top}, 800); // плавно переходим к блоку
+           
+        }    
+    }
+});
