4d838f05103b9bdae99eaa429bc9fa0851ea22ec
RaTeNo <rateno@mail.ru>
2023-09-27 11:36:31 +0300

    Update simulator.js

--- js/simulator.js
+++ js/simulator.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
     $('body').on('click', '.steps_stage .step_stage:not(.active)', function (e) {
-        var target =  $(e.target);        
+        var target =  $(e.target);
         if(target.closest(".step_stage_img").length !== 0)
         {
             return;
         }
         var date = new Date();
-        $(this).addClass("active").find(".icon_change use").attr("xlink:href","images/sprite.svg#training_ok");
+        // $(this).addClass("active").find(".icon_change use").attr("xlink:href","/images/sprite.svg#training_ok");
+        $(this).addClass("active").find(".icon:not(.icon_has_img) use").attr("xlink:href","/images/sprite.svg#training_ok");
         let min = date.getMinutes();
         if(min<10)
         {
@@ -18,7 +19,8 @@ $(document).ready(function() {
         if($(".steps_stage .step_stage:not(.active)").length==0)
         {
             let id = $(this).closest(".steps_data_js").data("id");
-            $(".inner_steps .step[data-id='"+id+"']").removeClass("error").addClass("complete").find("use").attr("xlink:href","images/sprite.svg#step_complete");
+            $(".inner_steps .step[data-id='"+id+"']").removeClass("error").addClass("complete").find("use").attr("xlink:href","/images/sprite.svg#step_complete");
+
         }
     });
 
@@ -35,14 +37,27 @@ $(document).ready(function() {
             $(".steps_data_js[data-id='"+id+"']").fadeIn(200).addClass("active");
         }, 200);
 
+        $(".step_items_data_js").fadeOut(200);
+        setTimeout(() => {
+            $(".step_items_data_js[data-id='"+id+"']").fadeIn(200);
+        }, 200);
+
+        setTimeout(() => {
+            if($(window).width()>1279)
+            {
+                const elHeight = $(".data_with_sidebar .data.active").outerHeight();
+                $(".data_with_sidebar .steps").outerHeight(elHeight);
+            }
+        }, 500);
+
         $(".comments_data_js").fadeOut(200);
         setTimeout(() => {
             $(".comments_data_js[data-id='"+id+"']").fadeIn(200);
         }, 200);
 
-        setTimeout(() => {    
+        setTimeout(() => {
             if($(window).width()>1279)
-            {        
+            {
                 const elHeight = $(".data_with_sidebar .data.active").outerHeight();
                 $(".data_with_sidebar .steps").outerHeight(elHeight);
             }
@@ -59,34 +74,34 @@ $(document).ready(function() {
 
 
             /*if($(".data_with_sidebar .steps").length>0)
-            {   
-                
+            {
+
                     var hash = window.location.hash+"00";
                     if(hash)
                     {
                         var top = $(hash).position().top; // получаем координаты блока
                         $('.inner_steps').animate({scrollTop: top}, 800); // плавно переходим к блоку
                     }
-               
-            }   */ 
+
+            }   */
         }
         else
         {
 
             /*if($(".data_with_sidebar .steps").length>0)
-            {                   
+            {
                 var hash = window.location.hash+"00";
                 if(hash)
                 {
                     var left = $(hash).position().left ; // получаем координаты блока
                     $('.inner_steps').animate({scrollLeft: left }, 800); // плавно переходим к блоку
                 }
-               
-            }  */  
+
+            }  */
         }
     }, 0)
 
-   
+
 });
 
 $(window).on('resize', () => {
@@ -98,19 +113,19 @@ $(window).on('resize', () => {
 
 
         /*if($(".data_with_sidebar .steps").length>0)
-        {   
-                
+        {
+
                 var hash = window.location.hash+"00";
                 if(hash)
                 {
                     var top = $(hash).position().top; // получаем координаты блока
                     $('.inner_steps').animate({scrollTop: top}, 800); // плавно переходим к блоку
                 }
-           
-        }   */ 
+
+        }   */
     }
     else
     {
-        $(".data_with_sidebar .steps").css("height", "auto");        
+        $(".data_with_sidebar .steps").css("height", "auto");
     }
 });
