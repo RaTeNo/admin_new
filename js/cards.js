a6c7d057dc4ba906061235f437f06c91289b5f1f
RaTeNo <rateno@mail.ru>
2024-03-04 16:44:10 +0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -2,27 +2,27 @@ $(() => {
 
     $(".js-show-podskazka").on("click", function(e){
     	e.preventDefault();
-    	$(".hint[data-type='podskazka']").show();
+    	$(".hint[data-type='podskazka']").fadeIn();
     	$(".card_item").hide();
     });	
 
     $(".js-show-answer").on("click", function(e){
     	e.preventDefault();
      	$(".hint").hide();
-    	$(".hint[data-type='answer']").show();
+    	$(".hint[data-type='answer']").fadeIn();
     	$(".card_item").hide();
     });	
 
     $(".js-show-quest").on("click", function(e){
     	e.preventDefault();
     	$(".hint").hide();
-    	$(".card_item").show();
+    	$(".card_item").fadeIn();
     });	 
 
     $(".js-show-more").on("click", function(e){
     	e.preventDefault();
     	$(this).closest(".hint_item").find(".hint_item-text").hide();
-    	$(this).closest(".hint_item").find(".hint_item-text-more").show();
+    	$(this).closest(".hint_item").find(".hint_item-text-more").fadeIn();
     });	   
 
     
