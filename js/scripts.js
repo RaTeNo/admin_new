90274f02e4695075f7329365db33e872ebcecb74
RaTeNo <rateno@mail.ru>
2024-04-07 11:56:45 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -207,7 +207,7 @@
 		$(".js-search").val("");
 		$(".title_faq").show();
 		$(".title_faq_result").hide();
-		let	list = $(".accordion_item .text_block_wrap");
+		let	list = $(".accordion_item");
 		list.each(function(index) {
 			$(this).show();
 		});
@@ -230,12 +230,12 @@
 
 		let	list = $(".accordion_item");
 		list.each(function(index) {
-			let label_html = $(this).html();
-			let label = $(this).text();
+			let label_html = $(this).find(".text_block_wrap").html();
+			let label = $(this).find(".text_block_wrap").text();
 			if (label.toLowerCase().indexOf(value.toLowerCase()) == -1) {
-				$(this).hide();
+				$(this).closest(".accordion_item").hide();
 			} else {
-				$(this).show();
+				$(this).closest(".accordion_item").show();
 				if(value.length>1)
 				{
 					var re = new RegExp(value, 'gi');	
