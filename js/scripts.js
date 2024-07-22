e5c929fbc4a14c43ac49c840349833def1f21a27
RaTeNo <rateno@mail.ru>
2024-07-22 11:47:57 +0300

    fff

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ WH = window.innerHeight || document.clientHeight || document.getElementsByTagNam
 $(() => {
 
 	
-	document.querySelectorAll('.comments .add_comment .submit_btn').forEach(button => 
+	document.querySelectorAll('.comment_button').forEach(button => 
 	    button.addEventListener('click', (e) => {
 	    	e.preventDefault(); 
 	    	button.classList.add("active");
+	    	button.innerHTML="<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'><circle fill='#fff' stroke='#fff' stroke-width='15' r='15' cx='40' cy='100'><animate attributeName='opacity' calcMode='spline' dur='2' values='1;0;1;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='-.4'></animate></circle><circle fill='#fff' stroke='#fff' stroke-width='15' r='15' cx='100' cy='100'><animate attributeName='opacity' calcMode='spline' dur='2' values='1;0;1;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='-.2'></animate></circle><circle fill='#fff' stroke='#fff' stroke-width='15' r='15' cx='160' cy='100'><animate attributeName='opacity' calcMode='spline' dur='2' values='1;0;1;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='0'></animate></circle></svg>";
+
 	    })
 	);
 
