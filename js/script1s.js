a79a8feb11ae236b094b8633845e50180f4e197f
RaTeNo <rateno@mail.ru>
2024-07-29 21:17:22 +0300

    ввввввв

--- js/script1s.js
+++ js/script1s.js
@@ -32,10 +32,10 @@ $(() => {
 	}
 
 
-	// $(".learning-paths .training-plan_item ul li span").click(function (e) {
-	// 	e.preventDefault();
-	// 	$(this).parent().toggleClass("active");
-	// });
+	$(".learning-paths .training-plan_item ul li span").click(function (e) {
+		e.preventDefault();
+		$(this).parent().toggleClass("active");
+	});
 
 
 	$('.clear_input').click(function (e) {
